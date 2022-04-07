7e58ee9ce5c9c1680a500aab663415efc5eebca5
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2022-04-07 06:52:12 +0600

    iubat

--- cgpa calculator.c
+++ cgpa calculator.c
@@ -141,6 +141,106 @@ int main()
         }
     
     }
+    if((math>=74 && math<=76)||(csc>=74 && csc<=76)||(art>=74 && art<=76)||(eng>=74 && eng<=76))
+    {
+        if(math>=74&& math<=76)
+        {
+            printf("Math= C\n");
+        }
+        if(csc>=74 && csc<=76)
+        {
+            printf("csc= C\n");
+        }
+        if(art>=74 && art<=76)
+        {
+            printf("art= C\n");
+        }
+        if(eng>=74 && eng<=76)
+        {
+            printf("eng= C\n");
+        }
+    
+    }
+    if((math>=70 && math<=73)||(csc>=70 && csc<=73)||(art>=70 && art<=73)||(eng>=70 && eng<=73))
+    {
+        if(math>=70&& math<=73)
+        {
+            printf("Math= C-\n");
+        }
+        if(csc>=70 && csc<=73)
+        {
+            printf("csc= C-\n");
+        }
+        if(art>=70 && art<=73)
+        {
+            printf("art= C-\n");
+        }
+        if(eng>=70 && eng<=73)
+        {
+            printf("eng= C-\n");
+        }
+    
+    }
+    if((math>=65 && math<=69)||(csc>=65 && csc<=69)||(art>=65&& art<=69)||(eng>=65 && eng<=69))
+    {
+        if(math>=65&& math<=69)
+        {
+            printf("Math= D+\n");
+        }
+        if(csc>=65 && csc<=69)
+        {
+            printf("csc= D+\n");
+        }
+        if(art>=65 && art<=69)
+        {
+            printf("art= D+\n");
+        }
+        if(eng>=65 && eng<=69)
+        {
+            printf("eng= D+\n");
+        }
+    
+    }
+    if((math>=60 && math<=64)||(csc>=60 && csc<=64)||(art>=60&& art<=64)||(eng>=60 && eng<=64))
+    {
+        if(math>=60&& math<=64)
+        {
+            printf("Math= D\n");
+        }
+        if(csc>=60 && csc<=64)
+        {
+            printf("csc= D\n");
+        }
+        if(art>=60 && art<=64)
+        {
+            printf("art= D\n");
+        }
+        if(eng>=60 && eng<=64)
+        {
+            printf("eng= D\n");
+        }
+    
+    }
+    if((math<60)||(csc<60)||(art<60)||(eng<60))
+    {
+        if(math<60)
+        {
+            printf("Math= F\n");
+        }
+        if(csc<60)
+        {
+            printf("csc= F\n");
+        }
+        if(art<60)
+        {
+            printf("art= F\n");
+        }
+        if(eng<60)
+        {
+            printf("eng= F\n");
+        }
+    
+    }
 
     return 0;
 }
